0957f914ebbb8ccb80661618e88a6f1897d4e389
assyadyu <assya.dyu@gmail.com>
2025-01-23 15:31:16 +0500

    fixtures for tests

---
+++ src/tests/conftest.py
@@ -0,0 +1,75 @@
+import asyncio
+from os import environ
+
+import pytest_asyncio
+from dotenv import load_dotenv
+from httpx import AsyncClient, ASGITransport
+
+from app.common import logger, settings
+from app.infrastructure.adapters import http_client
+from app.infrastructure.db.config import Base
+from app.infrastructure.db.sessions import async_engine, async_session
+from app.infrastructure.repositories.fake.orders import FakeOrderRepository
+from app.infrastructure.repositories.sqlalchemy import OrderRepository
+from app.interfaces.repositories import IOrderRepository
+from app.main import app
+
+load_dotenv()
+settings.PG_DB = environ.get("POSTGRES_DB")
+settings.PG_HOST = environ.get("POSTGRES_HOST")
+settings.PG_USER = environ.get("POSTGRES_USER")
+settings.PG_PASSWORD = environ.get("POSTGRES_PASSWORD")
+settings.REDIS_PORT = environ.get("REDIS_PORT")
+settings.REDIS_PORT = environ.get("REDIS_PORT")
+
+test_engine = async_engine(settings.db_url)
+new_session = async_session(test_engine)
+
+pytest_plugins = [
+    "tests.fixtures.users",
+]
+
+
+@pytest_asyncio.fixture(scope='session')
+async def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session")
+def fake_repo():
+    app.dependency_overrides[IOrderRepository] = FakeOrderRepository
+    yield
+    app.dependency_overrides[IOrderRepository] = OrderRepository
+
+
+def override_async_session():
+    logger.info("test session created")
+    yield new_session()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=False)
+async def test_http_client() -> AsyncClient:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url='http://test') as client:
+        yield client
+
+
+app.dependency_overrides[async_session] = override_async_session
+app.dependency_overrides[http_client] = test_http_client
+
+
+@pytest_asyncio.fixture(autouse=False, scope="module")
+async def prepare_test_db():
+    logger.warning("prepare_test_db")
+    logger.warning("CREATE TABLES")
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    await test_engine.dispose()
+
+    yield
+    logger.warning("DROP TABLES")
+    await test_engine.dispose()
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    await test_engine.dispose()
---
+++ src/tests/fixtures/users.py
@@ -0,0 +1,43 @@
+import uuid
+
+import pytest_asyncio
+
+from app.common.enums import UserRoleEnum
+from app.main import app
+from app.users.schemas import TokenPayload
+from app.users.services import get_current_user
+
+user_uuid = str(uuid.uuid4())
+admin_uuid = str(uuid.uuid4())
+
+
+async def get_fake_current_user():
+    return TokenPayload(uuid=user_uuid, username="user",
+                        role=UserRoleEnum.USER.value, is_active=True)
+
+
+async def get_fake_current_admin():
+    return TokenPayload(uuid=str(admin_uuid), username="admin",
+                        role=UserRoleEnum.ADMIN.value, is_active=True)
+
+
+async def switch_to_user():
+    app.dependency_overrides[get_current_user] = get_fake_current_user
+    # yield
+    # app.dependency_overrides[get_current_user] = get_current_user
+
+
+async def switch_to_admin():
+    app.dependency_overrides[get_current_user] = get_fake_current_admin
+    # yield
+    # app.dependency_overrides[get_current_user] = get_current_user
+
+
+@pytest_asyncio.fixture(scope="module")
+async def auth_as_admin():
+    await switch_to_admin()
+
+
+@pytest_asyncio.fixture(scope="module")
+async def auth_as_user():
+    await switch_to_user()
