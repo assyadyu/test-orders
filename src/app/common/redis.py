5348d7d54699ebfb0286c8cd022fb8b2604ec293
assyadyu <assya.dyu@gmail.com>
2025-01-23 15:34:59 +0500

    redis upd for mock

--- src/app/common/redis.py
+++ src/app/common/redis.py
@@ -1,11 +1,8 @@
-from functools import cache
-
 from redis import asyncio as aioredis
 
 from app.common import settings, logger
 
 
-@cache
 def redis():
     logger.warning("redis connection")
     return aioredis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
