4885458970880ee295c9ed882fb87d429fb3714b
assyadyu <assya.dyu@gmail.com>
2025-01-27 16:57:50 +0500

    fake repo tests

--- src/tests/integration/repo_test.py
+++ src/tests/integration/repo_test.py
@@ -1,5 +1,4 @@
 import uuid
-from uuid import uuid4
 
 import pytest
 import pytest_asyncio
@@ -8,12 +7,57 @@ from app.common import logger
 from app.common.enums import OrderStatus
 from app.common.exceptions import ObjectDoesNotExistException
 from app.infrastructure.db.models import OrderModel
+from app.infrastructure.repositories.fake.orders import FakeOrderRepository
 from app.infrastructure.repositories.sqlalchemy import OrderRepository
 from app.interfaces.repositories.base import IBaseRepository
+from app.orders.schemas import UserData, NewOrderWithProductsSchema
 from tests.conftest import (
     new_session,
     random_string,
 )
+
+
+
+class TestFakeOrderRepository:
+    repo: FakeOrderRepository
+    user: UserData
+    admin: UserData
+
+    async def create_new_order(self, user):
+        data = NewOrderWithProductsSchema(
+            customer_name=str(uuid.uuid4()),
+            products=[]
+        )
+        obj = await self.repo.create_order_with_products(user, data)
+        return obj
+
+    async def clear_repo(self):
+        self.repo.objects = []
+
+    @staticmethod
+    @pytest_asyncio.fixture(autouse=True, scope="class", loop_scope="session")
+    async def setup():
+        TestFakeOrderRepository.repo = FakeOrderRepository()
+        TestFakeOrderRepository.admin = UserData(user_id=uuid.uuid4(), is_admin=True)
+        TestFakeOrderRepository.user = UserData(user_id=uuid.uuid4(), is_admin=False)
+
+    @pytest.mark.asyncio(loop_scope="session")
+    async def test_auth_get_orders_as_admin(self):
+        await self.clear_repo()
+        logger.info("test_auth_get_orders_as_admin")
+        await self.create_new_order(self.user)
+        await self.create_new_order(self.admin)
+        result = await self.repo.filter_orders(user=self.admin)
+        assert len(result) == 2
+
+    @pytest.mark.asyncio(loop_scope="session")
+    async def test_auth_get_orders_as_user(self):
+        await self.clear_repo()
+        logger.info("test_auth_get_orders_as_user")
+        await self.create_new_order(self.user)
+        await self.create_new_order(self.admin)
+        result = await self.repo.filter_orders(user=self.user)
+        assert len(result) == 1
 
 
 class TestOrderRepository:
@@ -39,7 +83,7 @@ class TestOrderRepository:
     @pytest.mark.asyncio(loop_scope="session")
     async def test_get_by_id_fail(self):
         logger.info("OrderRepository test_get_by_id_fail")
-        non_existing_id = uuid4()
+        non_existing_id = uuid.uuid4()
         with pytest.raises(ObjectDoesNotExistException) as exception:
             await self.repo.get_by_id(object_id=non_existing_id)
         assert str(exception.value) == f"{OrderModel.__name__} object id {non_existing_id} not found"
