49bdc2c62b150ebfcbd90a53b70ed1b65566e9f4
assyadyu <assya.dyu@gmail.com>
2025-01-27 23:20:57 +0500

    new line

--- src/app/common/redis.py
+++ src/app/common/redis.py
@@ -6,7 +6,7 @@ from app.common import settings, logger
 def redis():
     """
     Create global redis connection
-    :return:
+    :return: Redis connection
     """
     return aioredis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
