2b46c43b594e93cabcdc996bf85d41174da4aa27
assyadyu <assya.dyu@gmail.com>
2025-01-28 14:54:03 +0500

    import fix

--- src/app/interfaces/repositories/__init__.py
+++ src/app/interfaces/repositories/__init__.py
@@ -1,9 +1,7 @@
 from app.interfaces.repositories.base import IBaseRepository
 from app.interfaces.repositories.orders import IOrderRepository
-from app.interfaces.repositories.users import IUserRepository
 
 __all__ = [
     "IBaseRepository",
     "IOrderRepository",
-    "IUserRepository",
 ]
